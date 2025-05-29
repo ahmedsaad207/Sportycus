7fb6c9936e4419090c36e26cbd5c652f06fc14f5
Ahmed Saad <medosaad2071@gmail.com>
2025-05-29 14:40:16 +0300

    first commit

---
+++ Sportycus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sportycus
+//
+//  Created by Ahmed Saad on 29/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
