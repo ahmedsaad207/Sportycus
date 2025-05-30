1e0b2fab40f54888167a84abae5de8072422a1db
Youssif <youssifn.mostafa@gmail.com>
2025-05-30 20:22:53 +0300

    fix 4

---
+++ Sportycus/Features/LeagueDetails/View/Cells/LatestEventsCell.swift
@@ -0,0 +1,17 @@
+//
+//  LatestEventsCell.swift
+//  Sportycus
+//
+//  Created by Youssif Nasser on 30/05/2025.
+//
+
+import UIKit
+
+class LatestEventsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
