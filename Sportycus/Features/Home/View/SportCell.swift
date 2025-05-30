6c2b8a7cc18bb9f45e5c5e4d0bd6240dd53a7416
Youssif <youssifn.mostafa@gmail.com>
2025-05-30 16:12:25 +0300

    setup cell nib (not done as ui)

---
+++ Sportycus/Features/Home/View/SportCell.swift
@@ -0,0 +1,17 @@
+//
+//  SportCell.swift
+//  Sportycus
+//
+//  Created by Youssif Nasser on 30/05/2025.
+//
+
+import UIKit
+
+class SportCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
