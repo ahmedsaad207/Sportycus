a4802d98c401ea67f43b408f55c225f6fe20458e
Youssif <youssifn.mostafa@gmail.com>
2025-05-30 16:14:15 +0300

    fix home conflict

---
+++ Sportycus/Features/Home/Model/CellID.swift
@@ -0,0 +1,17 @@
+//
+//  Routs.swift
+//  Sportycus
+//
+//  Created by Youssif Nasser on 30/05/2025.
+//
+
+import Foundation
+
+enum CellID: String, CustomStringConvertible {
+    case home
+    case details
+
+    var description: String {
+        return self.rawValue
+    }
+}
