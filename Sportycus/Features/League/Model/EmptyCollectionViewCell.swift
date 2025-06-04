7f75067a2c3cb89421c48eef435e314c21ad2e11
Youssif <youssifn.mostafa@gmail.com>
2025-06-04 03:06:17 +0300

    retrieved successfully

--- Sportycus/Features/League/Model/EmptyCollectionViewCell.swift
+++ Sportycus/Features/League/Model/EmptyCollectionViewCell.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Lottie
 
 class EmptyCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var animationView: LottieAnimationView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // 1. Set animation content mode
+         
+         animationView.contentMode = .scaleAspectFit
+         
+         // 2. Set animation loop mode
+         
+         animationView.loopMode = .loop
+         
+         // 3. Adjust animation speed
+         
+         animationView.animationSpeed = 0.5
+         
+         // 4. Play animation
+         animationView.play()
     }
 
 }
