6f8ed4a14eda891fe50a6d6cfeb6ea86d0cdbf4c
medosaad207 <medosaad2071@gmail.com>
2025-06-06 04:34:48 +0300

    add new test

---
+++ SportycusTests/SportycusTests.swift
@@ -0,0 +1,31 @@
+//
+//  SportycusTests.swift
+//  SportycusTests
+//
+//  Created by Ahmed Saad on 06/06/2025.
+//
+
+import XCTest
+
+final class SportycusTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(1, 1)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
